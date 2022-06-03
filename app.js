15f23fda2f243becc5cc89fa9aeb6edc25353c79
DavidKielty1 <davidkielty@live.co.uk>
2022-06-03 11:34:24 +0100

    Added default login

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config();
 }
 
-
-
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
@@ -162,7 +160,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`Serving on port ${port}`);
 });
